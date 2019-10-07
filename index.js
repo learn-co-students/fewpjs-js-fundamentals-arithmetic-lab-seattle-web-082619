ef254dbb4e9cbdcd1d07150f2a5b812a82c19cd3
ianhunterharold <ianhunterharold@gmail.com>
2019-10-07 11:28:39 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+let num1 = 1; 
+let num2 = 62;
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random() * 1000)
+random
+
+let four = 25 % 4
+four
+
+Math.max(1,2,3,10,20)
+
+
