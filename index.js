ed19fd73c29a816896d5b9216154eb23012a423d
Krista Loven <krista.loven.87@gmail.com>
2019-10-06 19:38:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+let num1 = 62;
+let num2 = 1;
+
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = 10 % 6;
+
+let max = Math.max(3, 20)
